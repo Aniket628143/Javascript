29cd69721b546c40e372df5c22a6cf8e49f886ee
Aniket628143 <aniket218321@gmail.com>
2026-05-03 17:01:07 +0000

    while and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+// let index = 1
+
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2
+// }
+
+
+// let myArray = ['alien-X', 'atomix', 'swampfire', 'bigchill', 'jetray']
+// let arr = 0
+
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr++
+// }
+
+let score = 1
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
