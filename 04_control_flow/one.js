cc8ed5789eb947bf7b59c8f251559a8c627556cc
Aniket628143 <aniket218321@gmail.com>
2026-05-03 16:11:26 +0000

    control flow statements, switch, truthy and falsy values in JS

---
+++ 04_control_flow/one.js
@@ -0,0 +1,50 @@
+const temperature = 41
+
+// if (temperature < 50) {
+//     console.log("Temperature is less than 50");
+// }else {
+//     console.log("Temperature is greater than 50");
+// }
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+
+// const score  = 200
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`User Power: ${power}`);
+// }
+// console.log(`User Power: ${power}`);
+
+const balance = 1000
+
+// if (balance > 500) console.log("test");
+// if (balance > 500) console.log("test"), console.log("test2");  // don't write code like this it is very unreadable
+
+// if (balance < 500){
+//     console.log("Less than 500");
+// }else if (balance < 750){
+//     console.log("Less than 750");
+// }else if (balance < 900){
+//     console.log("Less than 900");
+// }else{
+//     console.log("Less than 1200");
+// }
+
+
+const userLoggedIn = true
+const debitCard = true
+
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+// if (userLoggedIn && debitCard){
+//     console.log("Allow to buy courses");
+// }
+
+// if (loggedInFromGoogle || loggedInFromEmail){
+//     console.log("User logged in");
+    
+// }
+
